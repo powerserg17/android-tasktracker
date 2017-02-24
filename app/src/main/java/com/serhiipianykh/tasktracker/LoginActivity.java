6b4774c6812b4bf5ff223698866788e8164349d7
serhiipianykh <serhiipianykh@gmail.com>
2017-02-24 01:15:21 -0500

    single fragment activity model

--- app/src/main/java/com/serhiipianykh/tasktracker/LoginActivity.java
+++ app/src/main/java/com/serhiipianykh/tasktracker/LoginActivity.java
@@ -1,13 +1,13 @@
 package com.serhiipianykh.tasktracker;
 
+import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class LoginActivity extends AppCompatActivity {
+public class LoginActivity extends SingleFragmentActivity {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
+    protected Fragment createFragment() {
+        return LoginFragment.newInstance();
     }
 }
---
+++ app/src/main/java/com/serhiipianykh/tasktracker/LoginFragment.java
@@ -0,0 +1,26 @@
+package com.serhiipianykh.tasktracker;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by serhiipianykh on 2017-02-24.
+ */
+
+public class LoginFragment extends Fragment {
+
+    public static LoginFragment newInstance() {
+        LoginFragment fragment = new LoginFragment();
+        return fragment;
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.fragment_login, container, false);
+        return v;
+    }
+}
